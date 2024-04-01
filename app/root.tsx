d02715b2872ecd2feabb6975db242e9458a6e0ce
Li0329 <haxk1024@gmail.com>
2024-04-01 11:30:50 +0800

    feat: add vercel

--- app/root.tsx
+++ app/root.tsx
@@ -5,6 +5,7 @@ import {
   Scripts,
   ScrollRestoration,
 } from "@remix-run/react";
+import { Analytics } from '@vercel/analytics/react';
 
 import type { LinksFunction } from "@remix-run/node";
 import stylesheet from "~/tailwind.css?url";
@@ -26,6 +27,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
         {children}
         <ScrollRestoration />
         <Scripts />
+        <Analytics />
       </body>
     </html>
   );
